83b9e9d213aedbaf7dbbcc82cb4bf88766f3068d
Samuel Hurtado <samuelhurtado20@hotmail.com>
2022-08-02 23:43:02 -0500

    product details

---
+++ SystemInventory.Models/ViewModels/ShoppingCarViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemInventory.Models.ViewModels
+{
+    public class ShoppingCarViewModel
+    {
+        public Company Company { get; set; }
+        public WarehouseProduct WarehouseProduct { get; set; }
+        public ShoppingCar ShoppingCar { get; set; }
+    }
+}
--- SystemInventoryWebNet5/Areas/Inventory/Controllers/HomeController.cs
+++ SystemInventoryWebNet5/Areas/Inventory/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using SystemInventory.DataAccess.Repository.IRepository;
 using SystemInventory.Models;
 using SystemInventory.Models.ViewModels;
+using SystemInventoryWebNet5.DataAccess.Data;
 
 namespace SystemInventoryWebNet5.Areas.Inventory.Controllers
 {
@@ -13,17 +16,48 @@ namespace SystemInventoryWebNet5.Areas.Inventory.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _uow;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger, IUnitOfWork uow)
+        [BindProperty]
+        public ShoppingCarViewModel ShoppingCarVM { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork uow, ApplicationDbContext db)
         {
             _logger = logger;
             _uow = uow;
+            _db = db;
         }
 
         public IActionResult Index()
         {
             IEnumerable<Product> products = _uow.Product.GetAll(properties: "Category,Brand");
             return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            ShoppingCarVM = new ShoppingCarViewModel
+            {
+                Company = _db.Company.FirstOrDefault()                
+            };
+
+            ShoppingCarVM.WarehouseProduct = _db.WarehouseProduct.Include(prop => prop.Product).Include(prop => prop.Product.Category).Include(prop => prop.Product.Brand)
+                .FirstOrDefault(b => b.ProductId == id && b.WarehouseId == ShoppingCarVM.Company.Warehouse.Id);
+
+            if(ShoppingCarVM.WarehouseProduct == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ShoppingCarVM.ShoppingCar = new ShoppingCar()
+                {
+                    Product = ShoppingCarVM.WarehouseProduct.Product,
+                    ProductId = ShoppingCarVM.WarehouseProduct.ProductId
+                };
+
+                return View(ShoppingCarVM);
+            }
         }
 
         public IActionResult Privacy()
